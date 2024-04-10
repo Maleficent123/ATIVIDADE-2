4bd47e8fe81b3ba3e4505a77bade41ad55160ef3
maleficent123 <joseildabarbosalima@gmail.com>
2024-04-10 00:32:17 -0300

    Adiciona script executável

--- meu_script.sh
+++ meu_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Sua mensagem aqui"
+echo "Acorde todas as manhãs com um sorriso. Esta é mais uma oportunidade que você tem para ser feliz. Seja seu próprio motor de ignição. O dia de hoje jamais voltará .... Não o desperdice!!!"
